017be65b951ff18eb293225e2270d45e0eacffee
Azhaghiri <ganadhan6528@gmail.com>
2024-10-22 09:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function showAlert() {
+let firstName = document.getElementById("firname");
+let lastName = document.getElementById("lstname");
+let phoneNumbet = document.getElementById("num");
+let emailId = document.getElementById("email");
+
+	alert("First Name:" + firstName /n "Last Name" + lastName /n "Phone Number:" + phoneNumbet /n "Email ID" + emailId)
+}
